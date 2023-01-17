4e55f56c267524386dc38a6dd72f78bc58bfe362
titaniamega <megaintania8@gmail.com>
2023-01-17 16:32:27 +0700

    fixed harga

--- resources/views/umum/master.blade.php
+++ resources/views/umum/master.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -27,6 +28,9 @@
         // membuat Event Listener untuk memanggil fungsi initialize pada saat halaman selesai di load
         google.maps.event.addDomListener(window, 'load', initialize);
         </script>
+        <link href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
+        <link href="https://cdn.datatables.net/rowgroup/1.3.0/css/rowGroup.bootstrap4.min.css" rel="stylesheet" />
+        @yield('css')
     </head>
         
     <!-- Navigation-->
@@ -178,5 +182,11 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
         <script src="{{url('vendor/start/js/scripts.js')}}"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap4.min.js"></script>
+        <script src="https://cdn.datatables.net/rowgroup/1.3.0/js/dataTables.rowGroup.min.js"></script>
+        @yield('js')
     </body>
 </html>
+
