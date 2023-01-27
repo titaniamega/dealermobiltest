2a25fbce1f29f38aff0c54166d3c28b06d009cc3
titaniamega <megaintania8@gmail.com>
2023-01-27 14:56:27 +0700

    add alert

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -1,7 +1,7 @@
 @extends('adminlte::page')
 @section('title', 'Contact')
 @section('content_header')
-@include('sweetalert::alert')
+@include('sweetalert::alert', ['cdn' => "https://cdn.jsdelivr.net/npm/sweetalert2@9"])
     <h1 class="m-0 text-dark">Informasi Contact</h1>
 @stop
 @section('content')
