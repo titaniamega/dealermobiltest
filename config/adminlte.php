2d73d103096379dfa1476a67bd4dd376fe9fec99
titaniamega <megaintania8@gmail.com>
2023-01-27 15:32:39 +0700

    add alert

--- config/adminlte.php
+++ config/adminlte.php
@@ -479,7 +479,7 @@ return [
                 [
                     'type' => 'js',
                     'asset' => false,
-                    'location' => '//cdn.jsdelivr.net/npm/sweetalert2@8',
+                    'location' => '//cdn.jsdelivr.net/npm/sweetalert2@9',
                 ],
             ],
         ],
