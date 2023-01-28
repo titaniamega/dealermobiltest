69d5d93765434bb39d8c0c2fec3b6eeb69110359
titaniamega <megaintania8@gmail.com>
2023-01-28 15:40:03 +0700

    fixed form

--- resources/views/produk/create.blade.php
+++ resources/views/produk/create.blade.php
@@ -7,49 +7,46 @@
 @section('content')
     <form action="{{route('produk.store')}}" method="post" enctype="multipart/form-data">
         @csrf
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body">
+<div class="row">
+    <div class="col-12">
+        <div class="card">      
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-6">
                     <div class="form-group">
-                    <div class="row">
-                        <div class="col">
                         <label for="exampleInputpProduk">Nama Produk</label>
                         <input type="text" class="form-control @error('nama_produk') is-invalid @enderror" id="exampleInputProduk" placeholder="Nama Produk" name="nama_produk" value="{{old('nama_produk')}}">
                         @error('nama_produk') <span class="text-danger">{{$message}}</span> @enderror
-                        </div>
-                        <div class="col">
-                        <label for="exampleInputGambar">Gambar</label>
-                        <input type="file" class="form-control @error('gambar') is-invalid @enderror" id="exampleInputGambar" placeholder="Gambar mobil" name="gambar">
-                        @error('gambar') <span class="text-danger">{{$message}}</span> @enderror
-                        </div>
-                    </div>
                     </div>
                     <div class="form-group">
-                    <div class="row">
-                        <div class="col">
                         <label for="exampleInputHarga">Harga</label>
                         <input type="number" class="form-control @error('harga') is-invalid @enderror" id="exampleInputHarga" placeholder="Harga mobil" name="harga" value="{{old('harga')}}">
                         @error('harga') <span class="text-danger">{{$message}}</span> @enderror
-                        </div>
-                        <div class="col">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                <div class="form-group">
+                        <label for="exampleInputGambar">Gambar</label>
+                        <input type="file" class="form-control @error('gambar') is-invalid @enderror" id="exampleInputGambar" placeholder="Gambar mobil" name="gambar">
+                        @error('gambar') <span class="text-danger">{{$message}}</span> @enderror
+                    </div>
+                    <div class="form-group">
                         <label for="exampleInputGambarSlide">Gambar Slide</label>
                         <input type="file" class="form-control @error('gambarslide') is-invalid @enderror" id="exampleInputGambarSlide" placeholder="Gambar slide" name="gambarslide">
                         @error('gambarslide') <span class="text-danger">{{$message}}</span> @enderror
-                        </div>
                     </div>
-                    </div>
+                </div>
+                <div class="col-md-12">
                     <div class="form-group">
-                    <div class="row">
-                        <div class="col">
                         <label for="exampleInputDeskripsi">Deskripsi</label>
                         <textarea class="ckeditor form-control" name="deskripsi" id="deskripsi" rows="10" cols="30" value="{{old('deskripsi')}}"> </textarea>
                         <script>
                         CKEDITOR.replace('ckeditor');
                         </script>
-                        </div>
                     </div>
-                    </div>
+                </div>
+            </div>
+            </div>
                 <div class="card-footer">
                     <button type="submit" class="btn btn-primary">Simpan</button>
                     <a href="{{route('produk.index')}}" class="btn btn-default">
