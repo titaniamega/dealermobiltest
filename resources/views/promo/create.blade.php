992011713e7ee799174207abe8c4da652e3aa6ff
titaniamega <megaintania8@gmail.com>
2023-01-28 15:59:13 +0700

    fixed form

--- resources/views/promo/create.blade.php
+++ resources/views/promo/create.blade.php
@@ -7,13 +7,13 @@
 @section('content')
     <form action="{{route('promo.store')}}" method="post" enctype="multipart/form-data">
         @csrf
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body">
+<div class="row">
+    <div class="col-12">
+        <div class="card">      
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-6">
                     <div class="form-group">
-                    <div class="row">
-                        <div class="col">
                         <label for="exampleInputNama">Produk</label>
                         <select class="form-control" name="id_produk" id="select_produk" required>
                             <option value="" disabled selected>Pilih Model Produk</option>
@@ -21,38 +21,34 @@
                             <option value="{{$model->id}}"> {{$model->nama_produk}} </option>
                             @endforeach
                         </select>
-                        </div>
-                        <div class="col">
+                    </div>
+                    <div class="form-group">
                         <label for="exampleInputJudul">Judul Promo</label>
                         <input type="text" class="form-control @error('judul') is-invalid @enderror" id="exampleInputJudul" placeholder="Judul Promo" name="judul" value="{{old('judul')}}">
                         @error('judul') <span class="text-danger">{{$message}}</span> @enderror
-                        </div>
                     </div>
-                    </div>
+                </div>
+                <div class="col-md-6">
                     <div class="form-group">
-                    <div class="row">
-                        <div class="col">
                         <label for="exampleInputGambarPromo">Promo</label>
                         <input type="file" class="form-control @error('promo') is-invalid @enderror" id="exampleInputGambarPromo" placeholder="Gambar promo" name="gambar">
                         @error('promo') <span class="text-danger">{{$message}}</span> @enderror
-                        </div>
-                        <div class="col">
-                        <label for="date">Tanggal Berlaku</label>
-                        <input type="date" class="form-control" id="date" name="masa_berlaku" placeholder="Masa Berlaku">
-                        </div>
-                    </div>
                     </div>
                     <div class="form-group">
-                    <div class="row">
-                        <div class="col">
+                        <label for="date">Tanggal Berlaku</label>
+                        <input type="date" class="form-control" id="date" name="masa_berlaku" placeholder="Masa Berlaku">
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="form-group">
                         <label for="exampleInputKeterangan">Keterangan</label>
                         <textarea class="ckeditor form-control" name="keterangan" id="keterangan" rows="10" cols="30" value="{{old('keterangan')}}"> </textarea>
                         <script>
                         CKEDITOR.replace('ckeditor');
                         </script>
-                        </div>
                     </div>
-                    </div>
+                </div>
+            </div>
                 <div class="card-footer">
                     <button type="submit" class="btn btn-primary">Simpan</button>
                     <a href="{{route('promo.index')}}" class="btn btn-default">
