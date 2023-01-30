8b2b552e06a0b065ed2202a67bd0cb235da18a04
titaniamega <megaintania8@gmail.com>
2023-01-30 09:35:01 +0700

    fixed detail

--- resources/views/produk/show.blade.php
+++ resources/views/produk/show.blade.php
@@ -1,15 +1,14 @@
 @extends('adminlte::page')
 @section('title', 'Detail Produk')
 @section('content_header')
-    <h1>Detail Produk</h1>
+    <h1>Detail Produk : {{$produk->nama_produk}}</h1>
 @stop
 @section('content')
     <div class="row">
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    <div class="form-group">
-                        <div>
+                        <div class="form-group">
                             <div class="row mb-2">
                                 <div class="col-md-6 mb-2">
                                 <label>Tanggal : </label>
@@ -21,45 +20,35 @@
                                 </div>
                             </div>
                         </div>
-                    <div class="row">
-                        <div class="col">
-                        <label for="exampleInputName">Model</label>
-                        <input type="text" name="nama_produk" value="{{ $produk->nama_produk }}" class="form-control" readonly="readonly" placeholder="Nama Produk">
-                        </div>
-                        <div class="col">
-                        <label for="exampleInputHarga">Harga</label>
-                        <input type="number" name="harga" value="{{$produk->harga}}" class="form-control" placeholder="Harga Mobil" readonly="readonly">
-                        </div>
-                    </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="row">
-                            <div class="col">
-                                <div class="row">
-                                <label for="exampleInputGambar">Gambar</label>
+                        <div class="form-group">
+                            <div class="row mb-2">
+                                <div class="col-md-6 mb-2">
+                                <label>Model</label>
+                                <input type="text" name="nama_produk" value="{{ $produk->nama_produk }}" class="form-control" readonly="readonly" placeholder="Nama Produk">
                                 </div>
-                                <div class="row">
+                                <div class="col-md-6 mb-2">
+                                <label>Harga</label>
+                                <input type="number" name="harga" value="{{ $produk->harga }}" class="form-control" placeholder="Harga Mobil" readonly="readonly">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="row mb-2">
+                                <div class="col-md-6 mb-2">
+                                <label>Gambar</label>
                                 <img src="{{url('images/'.$produk->gambar)}}" class="img-fluid img-thumbnail"/> 
                                 </div>
-                            </div>
-                            <div class="col">
-                                <div class="row">
-                                <label for="exampleInputGambarSlide">Gambar Slide</label>
-                                </div>
-                                <div class="row">
-                                <img src="{{url('slides/'.$produk->gambarslide)}}" class="img-fluid img-thumbnail"/>  
-                                </div>
+                                <div class="col-md-6 mb-2">
+                                <label>Gambar Slide</label>
+                                <img src="{{url('slides/'.$produk->gambarslide)}}" class="img-fluid img-thumbnail"/>
+                                </div>  
                             </div>
                         </div>
-                    </div>
                     <div class="form-group">
-                    <div class="row">
-                        <div class="col">
-                        <label for="exampleInputDeskripsi">Deskripsi</label>
+                        <label>Deskripsi</label>
                         {!! $produk->deskripsi !!}
                         </div>
                     </div>
-                    </div>
+                </div>
             </div>
         </div>
     </div>
